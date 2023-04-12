d057e7e3464351e011dd424ebd3d4beded51a93b
DanilaZah <128993617+DanilaZah@users.noreply.github.com>
2023-04-12 23:14:58 +0500

    Add files via upload

---
+++ C++ labRab/119/V119.cpp
@@ -0,0 +1,33 @@
+﻿//1.19 Составьте программу для нахождения всех номеров счастливых билетов, у которых сумма первых (или последних) трех цифр, будучи возведенной в какую-либо степень,
+//равна номеру счастливого билета.
+
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <iterator>
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+    int i, j, k;
+    int ticket[6];
+    int temp_happyticket;
+    list <int> happyticket;
+
+    for (i = 0; i <= 999999; i++) {
+        for (k = i, j = 0; j < 6; j++, k /= 10) {
+            ticket[j] = k % 10;
+
+        }
+
+        if (pow(ticket[0] + ticket[1] + ticket[2], j) == i) {
+            happyticket.insert(happyticket.end(),i);
+            cout << i;
+
+        }
+        copy(happyticket.begin(), happyticket.end(), ostream_iterator<int>(cout, " "));
+        
+    }
+}
