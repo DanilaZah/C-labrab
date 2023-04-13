91b887b50960e6d4ee9c963a221105a91cf86117
DanilaZah <128993617+DanilaZah@users.noreply.github.com>
2023-04-13 23:03:21 +0500

    Add files via upload

--- C++ labRab/119/V119.cpp
+++ C++ labRab/119/V119.cpp
@@ -3,8 +3,7 @@
 
 #include <iostream>
 #include <cmath>
-#include <list>
-#include <iterator>
+
 using namespace std;
 
 
@@ -14,7 +13,7 @@ int main()
     int i, j, k;
     int ticket[6];
     int temp_happyticket;
-    list <int> happyticket;
+    
 
     for (i = 0; i <= 999999; i++) {
         for (k = i, j = 0; j < 6; j++, k /= 10) {
@@ -23,11 +22,10 @@ int main()
         }
 
         if (pow(ticket[0] + ticket[1] + ticket[2], j) == i) {
-            happyticket.insert(happyticket.end(),i);
-            cout << i;
+            cout << i << endl;
 
         }
-        copy(happyticket.begin(), happyticket.end(), ostream_iterator<int>(cout, " "));
+        
         
     }
 }
