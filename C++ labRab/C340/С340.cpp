8526ab5c294cee3a356a094e2ce1463f6869f968
DanilaZah <128993617+DanilaZah@users.noreply.github.com>
2023-05-11 20:44:12 +0500

    Add files via upload

--- C++ labRab/C340/С340.cpp
+++ C++ labRab/C340/С340.cpp
@@ -23,13 +23,28 @@ bool isPalindrome(string str)
     return true;
 }
 
+string eraser(string str)
+{
+    string clear_str;
+    int i;
+    int k;
+    int str_end = str.length();
+
+    for (i = 0, i < str_end; i++;) {
+        if (isalpha(str[i]) != 0) {
+            clear_str[k++] = str[i];
+        }
+    }
+    return clear_str;
+
+}
 
 int main()
 {
     string str;
     cin >> str;
 
-    if (isPalindrome(str) {
+    if (isPalindrome(str)==true {
         cout << "Palindrome";
     }
     else {
